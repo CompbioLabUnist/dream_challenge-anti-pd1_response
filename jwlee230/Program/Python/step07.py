29916a85dd0990de0a2cb15c8fbfb83e735e80d1
Jaewoong Lee <230@fumire.moe>
2021-01-11 19:58:39 +0900

    Add index

--- jwlee230/Program/Python/step07.py
+++ jwlee230/Program/Python/step07.py
@@ -30,20 +30,24 @@ if __name__ == "__main__":
     data = step00.read_pickle(args.input)
     float_data = data.select_dtypes(exclude=["object"])
     object_data = data.select_dtypes(include=["object"])
+    print(data)
 
     tsne_data = pandas.DataFrame(sklearn.manifold.TSNE(n_components=2, init="pca", random_state=0, method="exact", n_jobs=args.cpus).fit_transform(float_data), columns=["TSNE1", "TSNE2"])
 
     for column in tsne_data.columns:
         tsne_data[column] = sklearn.preprocessing.scale(tsne_data[column])
 
-    tsne_data = pandas.concat([object_data, tsne_data], axis="columns", join="inner", verify_integrity=True)
-    print(tsne_data)
+    tsne_data["index"] = list(data.index)
+    tsne_data.set_index("index", inplace=True)
+
+    final_data = pandas.concat([object_data, tsne_data], axis="columns", join="inner", verify_integrity=True)
+    print(final_data)
 
     seaborn.set(context="poster", style="whitegrid")
     for feature in sorted(object_data.columns):
         print(feature)
         fig, ax = matplotlib.pyplot.subplots(figsize=(24, 24))
-        seaborn.scatterplot(data=tsne_data, x="TSNE1", y="TSNE2", hue=feature, style=feature, ax=ax, legend="full")
+        seaborn.scatterplot(data=final_data, x="TSNE1", y="TSNE2", hue=feature, style=feature, ax=ax, legend="full")
         tar_files.append(feature + ".png")
         fig.savefig(tar_files[-1])
         matplotlib.pyplot.close(fig)
